4a747b29290d51858da0be30c4a4870b3506ec3f
Adam Ovadia <adam@aovadia.com>
2024-07-02 22:17:32 -0400

    Rework Bitstring addition to use bit_lists

--- BitstringAddition.py
+++ BitstringAddition.py
@@ -1,21 +1,21 @@
-def bit_addition(val1 : int, val2 : int) -> list:
+def bit_addition(val1 : list, val2 : list) -> list:
     """ 
     Function to compute bit addition
     Args:
-        val1 (int): The first integer to add (in binary representation)
-        val2 (int): The second integer to add (in binary representation)
+        val1 (bitlist): The first bitlist to add (in binary representation)
+        val2 (bitlist): The second bitlist to add (in binary representation)
 
     Returns:
         list: The addition result as a list of bits (most significant bit first)
     """
 
-    max_length = max(val1.bit_length(), val2.bit_length())
+    max_length = max(len(val1), len(val2))
     carry = 0
     # Initialze result bit string to the whichever val is larger + 1
     result = [0] * (max_length + 1)
     for num in range(max_length):
-        bit1 = (val1 >> num) & 1
-        bit2 = (val2 >> num) & 1
+        bit1 = val1[num]
+        bit2 = val2[num]
         (carry, result[num]) = sum_of_bits(bit1, bit2, carry)
     result[max_length] = carry
 
